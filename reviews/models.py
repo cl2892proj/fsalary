bf660270cf4ae333291c23be7213d72160f269be
junkseeker <pixiu521@gmail.com>
2016-03-21 23:16:44 -0400

    added two models for PERM and H1B, converted Hire to a view

--- reviews/models.py
+++ reviews/models.py
@@ -4,16 +4,23 @@ from django.db import models
 
 # Create your models here.
 
+
 class Hire(models.Model):
-    employer_name = models.CharField(max_length=128)
-    wage_rate = models.FloatField() 
-    wage_unit_of_pay = models.CharField(max_length=64,null=True)
-    job_title = models.CharField(max_length=128,null=True)
-    start_date = models.DateField()
+    year = models.IntegerField()
+    employer_name = models.TextField()
+    employer_address1 = models.TextField()
+    employer_address2 = models.TextField()
+    employer_city = models.TextField()
+    employer_state = models.TextField()
+    employer_postal_code = models.TextField()
+    job_title = models.TextField()
+    wage = models.FloatField()
+    unit_of_pay = models.TextField()
+    source = models.TextField()
 
-    def __unicode__(self):
-        return str(self.id)
-
+    class Meta:
+        managed = False
+        db_table = 'reviews_hire'
 
 class Review(models.Model):
     hire = models.ForeignKey(Hire)
@@ -22,3 +29,61 @@ class Review(models.Model):
     comment = models.CharField(max_length=400)
 
 
+class OflcH1B(models.Model):
+    year = models.IntegerField()
+    case_no = models.TextField()
+    case_status = models.TextField()
+    case_submitted = models.DateField()
+    employment_start_date = models.DateField()
+    employment_end_date = models.DateField()
+    employer_name = models.TextField()
+    employer_address1 = models.TextField()
+    employer_address2 = models.TextField()
+    employer_city = models.TextField()
+    employer_state = models.TextField()
+    employer_postal_code = models.TextField()
+    employer_country = models.TextField()
+    employer_province = models.TextField()
+    employer_phone = models.TextField()
+    employer_phone_ext = models.TextField()
+    job_title = models.TextField()
+    soc_name = models.TextField()
+    full_time_position = models.TextField()
+    prevailing_wage = models.FloatField()
+    pw_unit_of_pay = models.TextField()
+    wage_rate_of_pay_from = models.FloatField()
+    wage_rate_of_pay_to = models.FloatField()
+    wage_unit_of_pay = models.TextField()
+    worksite_city = models.TextField()
+    worksite_county = models.TextField()
+    worksite_state = models.TextField()
+    worksite_postal_code = models.TextField()
+    lca_case_workloc2_city = models.TextField()
+    lca_case_workloc2_state = models.TextField()
+    pw_2 = models.FloatField()
+    pw_unit_2 = models.TextField()
+
+class OflcPerm(models.Model):
+    year = models.IntegerField()
+    case_number = models.TextField()
+    decision_date = models.DateField()
+    case_status = models.TextField()
+    case_received_date = models.DateField()
+    employer_name = models.TextField()
+    employer_address_1 = models.TextField()
+    employer_address_2 = models.TextField()
+    employer_city = models.TextField()
+    employer_state = models.TextField()
+    employer_country = models.TextField()
+    employer_postal_code = models.TextField()
+    employer_num_employees = models.FloatField()
+    employer_yr_estab = models.FloatField()
+    pw_amount_9089 = models.FloatField()
+    pw_unit_of_pay_9089 = models.TextField()
+    wage_offer_from_9089 = models.FloatField()
+    wage_offer_to_9089 = models.FloatField()
+    wage_offer_unit_of_pay_9089 = models.TextField()
+    job_info_job_title = models.TextField()
+    job_info_education = models.TextField()
+    job_info_major = models.TextField()
+    class_of_admission = models.TextField()
